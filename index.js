62abe545f5b46174c4348e2cd5f46e3d1cd88f87
Artem Nazarenko <70914823+AvangardAA@users.noreply.github.com>
2024-01-15 21:17:19 +0200

    middleware to give access only for postman

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const port = 1234;
 
 const PostmanUserAgentStr = "PostmanRuntime/7.36.0";
 
-// easy debug trick used to identify postman request and then set middlewares
-app.get('/', (req, res) => {
-  if (req.get('User-Agent') === PostmanUserAgentStr)
-  {
-    console.log("this is postman");
+app.use((req, res, next) => {
+  const whitelist = [PostmanUserAgentStr];
+  if (whitelist.includes(req.headers['user-agent'])) {
+    res.setHeader('Allow-User-Agent', req.headers['user-agent']);
+    next();
   }
-  res.send('Hello KSE');
+  else 
+  {
+    res.status(403).send("denied");
+  }
 });
 
-
+// easy debug trick used to identify postman request and then set middlewares
+app.get('/', (req, res) => {
+  res.send('Hello KSE');
+});
 
 app.listen(port, () => {
   console.log('app on port 1234');
