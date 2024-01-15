41a73668f23c98a9d6276e0fed3f3f857f789a11
Artem Nazarenko <70914823+AvangardAA@users.noreply.github.com>
2024-01-15 21:10:16 +0200

    easy postman identification to set middlewares

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const port = 1234;
 
+const PostmanUserAgentStr = "PostmanRuntime/7.36.0";
+
+// easy debug trick used to identify postman request and then set middlewares
 app.get('/', (req, res) => {
+  if (req.get('User-Agent') === PostmanUserAgentStr)
+  {
+    console.log("this is postman");
+  }
   res.send('Hello KSE');
 });
+
+
 
 app.listen(port, () => {
   console.log('app on port 1234');
